03054cef0020d689e67c8616ec0b3d057b2bf6ae
Abraham Epton <abraham.epton@gmail.com>
2015-09-20 12:39:22 -0500

    Catching missing home handicap during live games

--- sportsdirect/boxscore.py
+++ sportsdirect/boxscore.py
@@ -27,8 +27,11 @@ class BoxScoreFeed(BaseFeed):
     def parse(self, xml_text):
         root = etree.fromstring(xml_text)
         c = root.xpath('//team-sport-content/league-content/season-content/competition')[0]
-        self.home_handicap = float(
-            c.xpath('./betting/point-spread[@closing="true"]/home-handicap/text()')[0])
+        try:
+            self.home_handicap = float(
+                c.xpath('./betting/point-spread[@closing="true"]/home-handicap/text()')[0])
+        except IndexError:
+            self.home_handicap = None
         try:
             self.is_finished = c.xpath(
                 './result-scope/competition-status/text()')[0].lower() == 'complete'
